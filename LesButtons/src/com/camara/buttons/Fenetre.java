65855ecbea95e1aacb4e158dc177137d9b15c196
Camara Laby Damaro <ldamaro98@gmail.com>
2019-05-13 05:18:35 +0200

    Layout manager Box et BoxLayout en Java

--- LesButtons/src/com/camara/buttons/Fenetre.java
+++ LesButtons/src/com/camara/buttons/Fenetre.java
@@ -1,49 +1,36 @@
 package com.camara.buttons;
 
-import java.awt.GridLayout; 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
- 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Fenetre extends JFrame{
 
   public Fenetre(){
-		
     this.setTitle("Box Layout");
     this.setSize(300, 120);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
 
-    JPanel b1 = new JPanel();
-    //On définit le layout en lui indiquant qu'il travaillera en ligne
-    b1.setLayout(new BoxLayout(b1, BoxLayout.LINE_AXIS));
+    //On crée un conteneur avec gestion horizontale
+    Box b1 = Box.createHorizontalBox();
     b1.add(new JButton("Bouton 1"));
-
-    JPanel b2 = new JPanel();
-    //Idem pour cette ligne
-    b2.setLayout(new BoxLayout(b2, BoxLayout.LINE_AXIS));
+    //Idem
+    Box b2 = Box.createHorizontalBox();
     b2.add(new JButton("Bouton 2"));
     b2.add(new JButton("Bouton 3"));
-
-    JPanel b3 = new JPanel();
-    //Idem pour cette ligne
-    b3.setLayout(new BoxLayout(b3, BoxLayout.LINE_AXIS));
+    //Idem
+    Box b3 = Box.createHorizontalBox();
     b3.add(new JButton("Bouton 4"));
     b3.add(new JButton("Bouton 5"));
     b3.add(new JButton("Bouton 6"));
-
-    JPanel b4 = new JPanel();
-    //On positionne maintenant ces trois lignes en colonne
-    b4.setLayout(new BoxLayout(b4, BoxLayout.PAGE_AXIS));
+    //On crée un conteneur avec gestion verticale
+    Box b4 = Box.createVerticalBox();
     b4.add(b1);
     b4.add(b2);
     b4.add(b3);
-		
+
     this.getContentPane().add(b4);
     this.setVisible(true);
-  }
+  }	
 }
