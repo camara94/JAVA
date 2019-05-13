f965dd2f377e96e837ac4c13ebcb7c4d17c05b96
Camara Laby Damaro <ldamaro98@gmail.com>
2019-05-13 05:32:03 +0200

    Layout manager Box et BorderLayout et animation en JFrame en Java

--- LesButtons/src/com/camara/buttons/Fenetre.java
+++ LesButtons/src/com/camara/buttons/Fenetre.java
@@ -1,102 +1,68 @@
 package com.camara.buttons;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Color; 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 public class Fenetre extends JFrame{
-
+  private Panneau pan = new Panneau();
+  private JButton bouton = new JButton("mon bouton");
+  private JPanel container = new JPanel();
+  
   public Fenetre(){
-    this.setTitle("GridBagLayout");
-    this.setSize(400, 360);
+    this.setTitle("Animation");
+    this.setSize(300, 300);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
-
-    //On crée nos différents conteneurs de couleur différente
-    JPanel cell1 = new JPanel();
-    cell1.setBackground(Color.YELLOW);
-    cell1.setPreferredSize(new Dimension(60, 40));		
-    JPanel cell2 = new JPanel();
-    cell2.setBackground(Color.red);
-    cell2.setPreferredSize(new Dimension(60, 40));
-    JPanel cell3 = new JPanel();
-    cell3.setBackground(Color.green);
-    cell3.setPreferredSize(new Dimension(60, 40));
-    JPanel cell4 = new JPanel();
-    cell4.setBackground(Color.black);
-    cell4.setPreferredSize(new Dimension(60, 40));
-    JPanel cell5 = new JPanel();
-    cell5.setBackground(Color.cyan);
-    cell5.setPreferredSize(new Dimension(60, 40));
-    JPanel cell6 = new JPanel();
-    cell6.setBackground(Color.BLUE);
-    cell6.setPreferredSize(new Dimension(60, 40));
-    JPanel cell7 = new JPanel();
-    cell7.setBackground(Color.orange);
-    cell7.setPreferredSize(new Dimension(60, 40));
-    JPanel cell8 = new JPanel();
-    cell8.setBackground(Color.DARK_GRAY);
-    cell8.setPreferredSize(new Dimension(60, 40));
-		
-    //Le conteneur principal
-    JPanel content = new JPanel();
-    content.setPreferredSize(new Dimension(300, 120));
-    content.setBackground(Color.WHITE);
-    //On définit le layout manager
-    content.setLayout(new GridBagLayout());
-		
-    //L'objet servant à positionner les composants
-    GridBagConstraints gbc = new GridBagConstraints();
-		
-    //On positionne la case de départ du composant
-    gbc.gridx = 0;
-    gbc.gridy = 0;
-    //La taille en hauteur et en largeur
-    gbc.gridheight = 1;
-    gbc.gridwidth = 1;
-    content.add(cell1, gbc);
-    //---------------------------------------------
-    gbc.gridx = 1;
-    content.add(cell2, gbc);
-    //---------------------------------------------
-    gbc.gridx = 2;		
-    content.add(cell3, gbc);		
-    //---------------------------------------------
-    //Cette instruction informe le layout que c'est une fin de ligne
-    gbc.gridwidth = GridBagConstraints.REMAINDER;
-    gbc.gridx = 3;	
-    content.add(cell4, gbc);
-    //---------------------------------------------
-    gbc.gridx = 0;
-    gbc.gridy = 1;
-    gbc.gridwidth = 1;
-    gbc.gridheight = 2;
-    //Celle-ci indique que la cellule se réplique de façon verticale
-    gbc.fill = GridBagConstraints.VERTICAL;
-    content.add(cell5, gbc);
-    //---------------------------------------------
-    gbc.gridx = 1;
-    gbc.gridheight = 1;
-    //Celle-ci indique que la cellule se réplique de façon horizontale
-    gbc.fill = GridBagConstraints.HORIZONTAL;
-    gbc.gridwidth = GridBagConstraints.REMAINDER;
-    content.add(cell6, gbc);
-    //---------------------------------------------
-    gbc.gridx = 1;
-    gbc.gridy = 2;
-    gbc.gridwidth = 2;
-    content.add(cell7, gbc);
-    //---------------------------------------------
-    gbc.gridx = 3;
-    gbc.gridwidth = GridBagConstraints.REMAINDER;
-    content.add(cell8, gbc);
-    //---------------------------------------------
-    //On ajoute le conteneur
-    this.setContentPane(content);
-    this.setVisible(true);		
+    container.setBackground(Color.white);
+    container.setLayout(new BorderLayout());
+    container.add(pan, BorderLayout.CENTER);
+    container.add(bouton, BorderLayout.SOUTH);
+    this.setContentPane(container);
+    this.setVisible(true);    
+    go();
   }
+  
+  private void go(){  
+    //Les coordonnées de départ de notre rond
+    int x = pan.getPosX(), y = pan.getPosY();
+    //Le booléen pour savoir si l'on recule ou non sur l'axe x
+    boolean backX = false;
+    //Le booléen pour savoir si l'on recule ou non sur l'axe y
+    boolean backY = false;
+    
+    //Dans cet exemple, j'utilise une boucle while
+    //Vous verrez qu'elle fonctionne très bien
+    while(true){
+      //Si la coordonnée x est inférieure à 1, on avance
+      if(x < 1)backX = false;
+      //Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
+      if(x > pan.getWidth()-50)backX = true;
+      //Idem pour l'axe y
+      if(y < 1)backY = false;
+      if(y > pan.getHeight()-50)backY = true;
+      
+      //Si on avance, on incrémente la coordonnée
+      if(!backX)
+        pan.setPosX(++x);
+      //Sinon, on décrémente
+      else
+        pan.setPosX(--x);
+      //Idem pour l'axe Y
+      if(!backY)
+        pan.setPosY(++y);
+      else
+        pan.setPosY(--y);
+        
+      //On redessine notre Panneau
+      pan.repaint();
+      //Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
+      try {
+        Thread.sleep(3);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }    
+  }  
 }
